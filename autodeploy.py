0119ad6601a250d91c3a28f61373129338b0b3f9
potterbl <zeexample42@gmail.com>
2023-12-12 02:44:01 +0200

    'auto-deploy'

--- autodeploy.py
+++ autodeploy.py
@@ -2,7 +2,6 @@
 
 import subprocess
 import time
-import pygame
 
 def deploy():
     subprocess.call(["git", "add", "."])
@@ -11,20 +10,8 @@ def deploy():
 
 if __name__ == "__main__":
     deploy_interval = 30 * 60
-    session = True
 
-    pygame.init()
-    clock = pygame.time.Clock()
-
-    while session:
+    while True:
         deploy()
         print("Проект успешно задеплоен.")
-
-        # Проверка нажатия клавиши 'x'
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_x:
-                print("Нажата клавиша 'x'. Остановка сессии.")
-                session = False
-
-        clock.tick(10)
         time.sleep(deploy_interval)
