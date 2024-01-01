40939fd2d2be12c400adbe6a9fa34d13e304eb54
potterbl <zeexample42@gmail.com>
2024-01-01 22:18:07 +0200

    little changes

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -186,6 +186,45 @@ const Reviews = () => {
                             <div className="reviews_slide-inner">
                                 <div className="reviews_slide-head">
                                     <div className="reviews_slide-head_avatar"
+                                         style={{backgroundImage: `url('${observatorna}')`}}>
+
+                                    </div>
+                                    <div className="reviews_slide-head-name">
+                                        <p className="body-text break-word">
+                                            Артем
+                                        </p>
+                                        <p className="break-word reviews_slide-head_desc">
+                                            Власник клубу на вул. Обсерваторна.
+                                        </p>
+                                    </div>
+                                </div>
+                                <hr className="reviews_slide-hr"/>
+                                <div className="reviews_slide-body">
+                                    <p className="body-text-bigger">
+                                    </p>
+                                    <p className="body-text">
+                                        <span className="span-yellow bold body-text-bigger">"</span>Спорт - це актуальна
+                                        сьогодні тема. Ми
+                                        поєднали дві сторони практичну і емоційну. Тому обрали кіберсопртивний бізнес і
+                                        франшизу Cyberion.<span className="span-yellow bold body-text-bigger">"</span>
+                                    </p>
+                                </div>
+                                <hr className="reviews_slide-hr"/>
+                                <div className="reviews_slide-footer">
+                                    <a href="https://youtu.be/4Uw_X6dLmfQ?si=HLNskRKUg2948lJ8"
+                                       className="body-text underline">Дивитись відео на YouTube↗</a>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <motion.div
+                            className="reviews_slide"
+                            variants={reviewAnimation}
+                        >
+                            <div className="reviews_slide-inner">
+                                <div className="reviews_slide-head">
+                                    <div className="reviews_slide-head_avatar"
                                          style={{backgroundImage: `url('${darnitsya}')`}}>
 
                                     </div>
@@ -212,6 +251,46 @@ const Reviews = () => {
                                 <hr className="reviews_slide-hr"/>
                                 <div className="reviews_slide-footer">
                                     <a href="https://youtu.be/xeTR58KmmQY?si=oqARvfnSYucGWlFx"
+                                       className="body-text underline">Дивитись відео на YouTube↗</a>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <motion.div
+                            className="reviews_slide"
+                            variants={reviewAnimation}
+                        >
+                            <div className="reviews_slide-inner">
+                                <div className="reviews_slide-head">
+                                    <div className="reviews_slide-head_avatar"
+                                         style={{backgroundImage: `url('${politech}')`}}>
+
+                                    </div>
+                                    <div className="reviews_slide-head-name">
+                                        <p className="body-text break-word">
+                                            Віталій
+                                        </p>
+                                        <p className="break-word reviews_slide-head_desc">
+                                            Власник клубу на пр. Перемоги.
+                                        </p>
+                                    </div>
+                                </div>
+                                <hr className="reviews_slide-hr"/>
+                                <div className="reviews_slide-body">
+                                    <p className="body-text-bigger">
+                                    </p>
+                                    <p className="body-text">
+                                        <span className="span-yellow bold body-text-bigger">"</span>Кіберспорт наразі є
+                                        дуже перспективним напрямком де поки відсутня конкуренція на ринку. А також
+                                        зустрів вас в яких вже є мережа кіберклубів, а значить ви продаєте досвід, а не
+                                        повітря.<span
+                                        className="span-yellow bold body-text-bigger">"</span>
+                                    </p>
+                                </div>
+                                <hr className="reviews_slide-hr"/>
+                                <div className="reviews_slide-footer">
+                                    <a href="https://youtu.be/cNtZtW_HhoI?si=oZfNiDcuB9pURkht"
                                        className="body-text underline">Дивитись відео на YouTube↗</a>
                                 </div>
                             </div>
