65ebafaf0ac69d5f8a4586c8e0eecd20bc409be8
potterbl <zeexample42@gmail.com>
2024-01-27 04:16:58 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import FranchisePage from "./pages/FranchisePage";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
@@ -9,12 +8,7 @@ function App() {
     return (
         <Provider store={store}>
             <div className="App">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path={'/'} element={<FranchisePage/>}/>
-                        <Route path={'*'} element={<Navigate to={'/'}/>}/>
-                    </Routes>
-                </BrowserRouter>
+                <FranchisePage/>
             </div>
         </Provider>
     );
